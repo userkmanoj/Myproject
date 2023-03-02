c6c2756fd52a5b6b4adf8e55db2c42d12c1c2bba
userkmanoj <infokmanoj@gmail.com>
2023-03-02 12:03:06 +0530

    code added

--- src/org/sample/New.java
+++ src/org/sample/New.java
@@ -5,6 +5,9 @@ public class New {
 public static void main(String[] args) {
 	
 	System.out.println("Java project");
+	System.out.println("Phython project");
+	System.out.println("Selinium project");
+	System.out.println("Testng project");
 	
 	
 }
