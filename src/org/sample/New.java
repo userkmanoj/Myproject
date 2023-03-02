32f401fd6256a9541d4ce75b6affdaa0f47700aa
userkmanoj <infokmanoj@gmail.com>
2023-03-02 15:29:20 +0530

    code added

--- src/org/sample/New.java
+++ src/org/sample/New.java
@@ -5,6 +5,10 @@ public class New {
 public static void main(String[] args) {
 	
 	System.out.println("Java project");
+	System.out.println("Java project");
+	System.out.println("Java project");
+	System.out.println("Java project");
+	System.out.println("Java project");
 	
 	
 }
